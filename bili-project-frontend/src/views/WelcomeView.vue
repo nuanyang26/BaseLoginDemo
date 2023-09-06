216f44092ea04f9eeea23f5c4a19279c35621a42
nuanyang26 <1090936383@qq.com>
2023-09-06 21:55:31 +0800

    P9-引入element-plus，配置自动导入，完成WelCome左侧页面搭建（字体大小位置调整）。

--- bili-project-frontend/src/views/WelcomeView.vue
+++ bili-project-frontend/src/views/WelcomeView.vue
@@ -5,7 +5,14 @@
 <template>
   <div class="box">
     <div class="left-card">
+      <el-image fit="cover" src="/src/assets/img/20230401_WelComeImg.jpg"
+                style="width: 100%;height: 100%;"></el-image>
+    </div>
 
+    <div class="welcome-title">
+      <div style="font-size: 30px; font-weight: bold;">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里你可以学习如何使用Java，如何搭建网站，并且与Java之父密切交流</div>
+      <div style="margin-top: 5px">在这里你可以同性交友，因为都是男的，没有学Java的女生</div>
     </div>
 
     <div class="right-card">
@@ -31,5 +38,13 @@
   width: 400px;
   z-index: 1;
   background-color: aliceblue;
+}
+
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
 }
 </style>
